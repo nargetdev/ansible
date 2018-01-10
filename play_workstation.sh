55548324c671aab9e75a8ec18bf1d50cd9d59211
Nathan Argetsinger <narget@umich.edu>
2018-01-09 17:32:10 -0800

    this is basically a dump of recent stuff.  Might not be perfectly organized.

--- play_workstation.sh
+++ play_workstation.sh
@@ -1,2 +1,6 @@
-ansible-playbook -i RPis/testing workstation.yml --ask-become-pass
+# ansible-playbook -i RPis/testing workstation.yml --ask-become-pass
 #ansible-playbook -i RPis/testing workstation.yml
+
+
+# ansible-playbook -i hosts workstation.yml --ask-become-pass
+ansible-playbook -i hosts workstation.yml
